1376a3d1f3ef38672e07a68f618c73dbcd602206
Miguel <rojassucarinomiguelantoni@gmail.com>
2024-05-03 14:02:51 -0400

    MODIFICANDO CODIGO DE JAVASCRIPT

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIZ', 'CUMPLE','Papi','TE AMO♥' ],
+			strings: [ 'FELIZ', 'CUMPLE','PAPI','TE AMO♥' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
