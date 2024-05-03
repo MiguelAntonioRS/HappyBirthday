3ca043c8cbe61271859890add3c4f8c09e2b2921
Miguel <rojassucarinomiguelantoni@gmail.com>
2024-05-03 13:59:14 -0400

    Re Cambiando MENSAJE

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIZ', 'CUMPLE','SHEI','TE AMO♥' ],
+			strings: [ 'FELIZ', 'CUMPLE','Papi','TE AMO♥' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
