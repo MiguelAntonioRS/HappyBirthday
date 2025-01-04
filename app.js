77ffc48f108ab93f8d84af3d8c9833e2a68ae7f5
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-04 13:08:05 -0500

    SAME

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIZ', 'CUMPLE','FIDEL♥'],
+			strings: [ 'FELIZ', 'CUMPLE','PAPI♥'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
